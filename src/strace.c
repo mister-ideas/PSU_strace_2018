7a2c6a73de01d1cf11c3e5e671beec8eb400525e
Vincent Attali <vincent.attali@epitech.eu>
2019-05-04 12:00:08 +0200

    Fixed if

--- src/strace.c
+++ src/strace.c
@@ -67,7 +67,8 @@ int main(int ac, char **av, char **envp)
     if (ac == 1 || ac > 4) {
         dprintf(2, "USAGE: ./strace [-s] [-p <pid>|<command>]\n");
         return (84);
-    } else if (strcmp(av[1], "--help") == 0)
+    }
+    if (strcmp(av[1], "--help") == 0)
         printf("USAGE: ./strace [-s] [-p <pid>|<command>]\n");
     else if (strcmp(av[1], "-s") == 0) {
         hexadecimal = 0;
